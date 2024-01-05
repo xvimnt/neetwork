db044ef2e035e3dc26f657a6e41749630a3847e4
Xavi Monterroso <jvmonteros98@gmail.com>
2024-01-04 18:38:48 -0600

    Add loading animations to UILoader and UIPage404

--- src/components/UI/UILoader.tsx
+++ src/components/UI/UILoader.tsx
@@ -1,8 +1,22 @@
 import Image from "next/image";
 import React from "react";
 import LoadingGif from "../../assets/img/loading.gif";
+import LoaderGif from "../../assets/img/loader.gif";
 
 export const UILoader = (props: { size?: number }) => {
+  return (
+    <div className="h-full w-full items-center justify-center">
+      <Image
+        src={LoaderGif}
+        width={props.size ?? 500}
+        height={props.size ?? 500}
+        alt="loading"
+      />
+    </div>
+  );
+};
+
+export const UILoadingTurtle = (props: { size?: number }) => {
   return (
     <Image
       src={LoadingGif}
@@ -16,7 +30,7 @@ export const UILoader = (props: { size?: number }) => {
 export const UILoadingPage = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <UILoader />
+      <UILoadingTurtle />
     </div>
   );
 };
